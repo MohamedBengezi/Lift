0b293d7f9669bd7ff4f5ab2d457390b3f1100819
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-11-11 22:33:16 -0500

    Adding transitions to/from Feed & Profile Screens

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator, TransitionPresets } from 'react-navigation-stack';
 import Header from './src/components/Header';
 import HeaderLeft from './src/components/HeaderLeft';
 import HeaderRight from './src/components/HeaderRight';
@@ -12,9 +12,24 @@ import { Provider } from './src/components/context/CreateContext';
 
 const navigator = createStackNavigator(
   {
-    Main: MainScreen,
-    Feed: HomeScreen,
-    Profile: ChatScreen,
+    Main: {
+      screen: MainScreen,
+      navigationOptions: ({ navigation }) => ({
+        ...TransitionPresets.ModalSlideFromBottomIOS,
+      }),
+    },
+    Feed: {
+      screen: HomeScreen,
+      navigationOptions: ({ navigation }) => ({
+        ...TransitionPresets.ModalSlideFromBottomIOS,
+      }),
+    },
+    Profile: {
+      screen: ChatScreen,
+      navigationOptions: ({ navigation }) => ({
+        ...TransitionPresets.ModalSlideFromBottomIOS,
+      }),
+    },
     Create: CreateScreen
   },
   {
@@ -24,9 +39,6 @@ const navigator = createStackNavigator(
         headerStyle: {
           backgroundColor: '#ffffff',
         },
-        headerTitle: () => (<Header onPress={() => navigation.navigate('Main')} />),
-        headerLeft: () => (<HeaderLeft onPress={() => navigation.navigate('Feed')} />),
-        headerRight: () => (<HeaderRight onPress={() => navigation.navigate('Profile')} />)
       }
     }
   }
