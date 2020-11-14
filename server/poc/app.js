496b8d6b0ddf3d9f1f8f7d22035957058f306902
Sagar Thomas <sagarthomas27@gmail.com>
2020-11-14 12:31:11 -0500

    Add basic image uploads and video streaming

--- server/poc/app.js
+++ server/poc/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 
-const app = express();
-
-app.get("/video", (req, res) => {
-  res.sendFile("assets/sample.mp4", { root: __dirname });
+// Set up disk storage system. Field name is based of the field value expected from the form-data,
+// which will be myFile for now. Files are stored locally. Will move to services such as aws s3 later.
+const storage = multer.diskStorage({
+  destination: "./public/uploads",
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + path.extname(file.originalname));
+  },
 });
 
-app.get("/sample", (req, res) => {
-  const path = "assets/sample.mp4";
+const upload = multer({
+  storage: storage,
+}).single("myFile");
+
+// Helper function for video stream. This will sends chunks of a video at a time instead of all at once
+const streamVideo = (req, res, path) => {
   const stat = fs.statSync(path); // Gets info about the file
   const fileSize = stat.size;
   const range = req.headers.range;
@@ -38,6 +46,43 @@ app.get("/sample", (req, res) => {
     res.writeHead(200, head);
     fs.createReadStream(path).pipe(res);
   }
+};
+
+const app = express();
+
+// Generic upload route. Can uplaod images or videos through here. All files are names myFile.jpg (or .mp4).
+// We only store one file of a specific type for now (will change later)
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.log("Error uploading file");
+    } else {
+      res.send("Upload success");
+    }
+  });
+});
+
+// Fetches a file with a specified name. For now, all files will be named 'myFile.(*)' so only request that. Extentsion must be passed for this to work
+app.get("/fetch/:file", (req, res) => {
+  const fileName = req.params.file;
+  const filePath = "./public/uploads/" + fileName;
+  // Assuming all images are jpg and everything else coming in is a video file. Will make this more robust later
+  if (path.extname(fileName) !== ".jpg") {
+    streamVideo(req, res, filePath);
+  } else {
+    res.sendFile(filePath, { root: __dirname });
+  }
+});
+
+// Test route. Sends a random sample video all at once (not streamed).
+app.get("/video", (req, res) => {
+  res.sendFile("assets/sample.mp4", { root: __dirname });
+});
+
+// Test route. Sends a video via stream
+app.get("/sample", (req, res) => {
+  const path = "assets/sample.mp4";
+  streamVideo(req, res, path);
 });
 
 app.listen(4000, () => {
