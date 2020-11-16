113eb266c41fa647cf9e35fb74c14d42f1adf50f
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-11-16 13:05:47 -0500

    Adding splash screen support

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -16,7 +16,8 @@ import * as Permissions from "expo-permissions";
 import { FlingGestureHandler, Directions } from "react-native-gesture-handler";
 import HeaderLeft from "../components/HeaderLeft";
 import HeaderRight from "../components/HeaderRight";
-import Ionicons from "react-native-vector-icons/Ionicons";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import * as SplashScreen from 'expo-splash-screen';
 
 const MainScreen = ({ navigation }) => {
   const [camera, setCamera] = useState({
@@ -32,7 +33,19 @@ const MainScreen = ({ navigation }) => {
   const [video, setVideo] = useState(null);
 
   useEffect(() => {
-    async function fetchPermissions() {
+    async function displaySplashWhileLoading() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+      fetchPermissions();
+    }
+    displaySplashWhileLoading()
+  }, []);
+
+  async function fetchPermissions() {
+    try {
       const { status } = await Permissions.askAsync(Permissions.CAMERA);
       setCamera((prevState) => ({
         ...prevState,
@@ -45,9 +58,12 @@ const MainScreen = ({ navigation }) => {
         ...prevState,
         hasAudioPermissions: status2 === "granted",
       }));
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      await SplashScreen.hideAsync();
     }
-    fetchPermissions();
-  }, []);
+  }
 
   takePicture = () => {
     if (this.camera) {
@@ -89,18 +105,15 @@ const MainScreen = ({ navigation }) => {
     if (image != null) {
       return (
         <View>
-          <Image source={{ uri: image.uri }} style={styles.preview} />
-          <Ionicons
-            name="md-send"
-            style={styles.post}
-            onPress={() => navigation.navigate("Feed", { image })}
+          <Image
+            source={{ uri: image.uri }}
+            style={styles.preview}
           />
+          <Ionicons name="md-send" style={styles.post} onPress={() => navigation.navigate('Feed', { image, video })} />
 
-          <Ionicons
-            name="md-backspace"
-            onPress={() => setImage(null)}
-            style={styles.cancel}
-          />
+
+          <Ionicons name="md-backspace" onPress={() => setImage(null)} style={styles.cancel} />
+
         </View>
       );
     } else if (video != null) {
@@ -118,60 +131,49 @@ const MainScreen = ({ navigation }) => {
             isLooping
             style={styles.preview}
           />
-          <Ionicons
-            name="md-send"
-            style={styles.post}
+          <Ionicons name="md-send" style={styles.post}
             onPress={() => {
               //UPLOAD HERE
               navigation.navigate("Feed", { video })
             }}
           />
 
-          <Ionicons
-            name="md-backspace"
-            onPress={() => setVideo(null)}
-            style={styles.cancel}
-          />
+
+          <Ionicons name="md-backspace" onPress={() => setVideo(null)} style={styles.cancel} />
         </View>
       );
     } else {
       return (
         <View style={{ flex: 1 }}>
-          <Camera
-            style={{ flex: 1 }}
-            type={camera.type}
-            ref={(ref) => {
-              this.camera = ref;
-            }}
-          >
+
+          <Camera style={{ flex: 1 }} type={camera.type} ref={(ref) => { this.camera = ref }} >
             <View
               style={{
                 flex: 1,
-                backgroundColor: "transparent",
-                flexDirection: "column",
-              }}
-            >
+                backgroundColor: 'transparent',
+                flexDirection: 'column',
+              }}>
               <TouchableOpacity
                 style={{
                   flex: 0.1,
-                  alignSelf: "flex-end",
-                  alignItems: "center",
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
                 }}
                 onPress={() => {
                   setCamera({
                     type:
-                      camera.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
+                      camera.type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back,
                   });
                 }}
+
               >
-                <Text style={styles.flip}>
+                <Text style={styles.flip} >
                   <Ionicons name="md-camera" style={styles.flip} />
                 </Text>
               </TouchableOpacity>
             </View>
             <View style={styles.menu}>
+
               <TouchableHighlight
                 style={styles.capture}
                 onPress={takePicture}
@@ -181,10 +183,11 @@ const MainScreen = ({ navigation }) => {
               >
                 <View />
               </TouchableHighlight>
+
             </View>
           </Camera>
         </View>
-      );
+      )
     }
   }
 };
@@ -197,51 +200,53 @@ MainScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#000",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
   },
   preview: {
-    alignItems: "center",
-    height: Dimensions.get("window").height,
-    width: Dimensions.get("window").width,
-    resizeMode: "cover",
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    resizeMode: 'cover'
+
   },
   capture: {
     width: 70,
     height: 70,
     borderRadius: 35,
     borderWidth: 5,
-    borderColor: "#FFF",
+    borderColor: '#FFF',
     marginBottom: 50,
-    alignSelf: "center",
+    alignSelf: 'center',
   },
   cancel: {
-    position: "absolute",
+    position: 'absolute',
     left: 20,
     top: 40,
-    color: "#ffffff",
-    fontWeight: "600",
+    color: '#ffffff',
+    fontWeight: '600',
     fontSize: 28,
   },
   post: {
-    position: "absolute",
+    position: 'absolute',
     right: 20,
     top: 40,
-    color: "#ffffff",
-    fontWeight: "600",
+    color: '#ffffff',
+    fontWeight: '600',
     fontSize: 28,
   },
   flip: {
     fontSize: 28,
     marginTop: 30,
     marginRight: 30,
-    color: "white",
+    color: 'white'
   },
   menu: {
-    justifyContent: "space-around",
-    flexDirection: "row",
-  },
+    justifyContent: 'space-around',
+    flexDirection: "row"
+
+  }
 });
 
 export default MainScreen;
