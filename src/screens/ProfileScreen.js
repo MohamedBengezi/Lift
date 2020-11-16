8119b376dea48e9eaef253ad8aea5e76f81345ac
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-11-15 22:33:59 -0500

    Reverting profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -262,9 +262,20 @@ class Profile extends Component {
 
     render() {
         return (
-            <View>
-
-            </View>
+            <ScrollView style={styles.scroll}>
+                <View style={[styles.container, this.props.containerStyle]}>
+                    <View style={styles.cardContainer}>
+                        {this.renderContactHeader()}
+                        <TabView
+                            style={[styles.tabContainer, this.props.tabContainerStyle]}
+                            navigationState={this.state.tabs}
+                            renderScene={this.renderScene}
+                            renderTabBar={this.renderTabBar}
+                            onIndexChange={this.handleIndexChange}
+                        />
+                    </View>
+                </View>
+            </ScrollView>
         )
     }
 }
