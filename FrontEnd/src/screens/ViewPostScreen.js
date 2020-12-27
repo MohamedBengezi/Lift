9cfa637cfdb4f5ffdab5146b1f7200ea75bda246
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-12-27 12:45:30 -0500

    Creating viewpost screen

---
+++ FrontEnd/src/screens/ViewPostScreen.js
@@ -0,0 +1,17 @@
+import React, { useReducer } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Button from '../components/common/Button';
+
+const ViewPostScreen = () => {
+    return (
+        <View>
+            <Text>List Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ViewPostScreen;
