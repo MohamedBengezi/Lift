c0a44f8d4fcbb16c4d975d9de152a595614c827f
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-12-28 21:38:26 -0500

    viewing post in separate page

--- FrontEnd/src/screens/ViewPostScreen.js
+++ FrontEnd/src/screens/ViewPostScreen.js
@@ -1,17 +1,24 @@
 import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Button from '../components/common/Button';
+import { navigate } from "../navigationRef";
+import { WebView } from "react-native-webview";
 
-const ViewPostScreen = () => {
+
+const ViewPostScreen = (props) => {
+    let post = props.navigation.state.params.post
     return (
-        <View>
-            <Text>List Screen</Text>
+        <View style={styles.post} >
+            {post}
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-
+    post: {
+        flex: 1,
+        alignItems: 'center'
+    },
 });
 
 export default ViewPostScreen;
