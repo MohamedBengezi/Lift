fc33283ca92e27fc195a2ebaf04aad162f7c60e3
Sagar Thomas <sagarthomas@xps.hitronhub.home>
2021-01-04 12:46:28 -0500

    Add test script to package.json

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -10,3 +10,17 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+export const createFeedbackPost = functions.https.onRequest(
+  (request, response) => {
+    functions.logger.info("create feedback post");
+    console.log(request.body.caption);
+    let userID = request.body.userID;
+    let caption = request.body.caption;
+    let topics = request.body.topics;
+
+    // admin.firestore().doc("feedback_posts/")
+
+    response.send("done");
+  }
+);
