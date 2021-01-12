cf0de5fa4ef929fd05f96987f9f9840d607f2d11
Matthew-Dombrady <matthew.dombrady@gmail.com>
2021-01-12 15:19:00 -0500

    Create/get user routes

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -11,7 +11,7 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const createFeedbackPost = functions.https.onRequest(
+/* export const createFeedbackPost = functions.https.onRequest(
   (request, response) => {
     functions.logger.info("create feedback post");
     console.log(request.body.caption);
@@ -24,3 +24,66 @@ export const createFeedbackPost = functions.https.onRequest(
     response.send("done");
   }
 );
+*/
+
+export const addUser = functions.https.onRequest((request, response) => {
+
+  let username = request.body.username;
+  let topics = request.body.topics;
+  let community_rating = request.body.community_rating;
+  let following = request.body.following;
+
+  let newUser = false
+
+  const usersRef = admin.firestore().collection('users');
+  usersRef.where('username', '==', username).get()
+  .then(data => {
+    if (data.size == 0) {
+      newUser = true
+    }
+  })
+  .catch(error =>
+    console.log(error)
+  )
+
+  if (newUser) {
+    admin.firestore().collection("users").add({data: []})
+    .then(data => {
+      return data.set({
+        username: username,
+        topics: topics,
+        community_rating: community_rating,
+        following: following,
+      })
+    })
+    .catch(error =>
+      console.log(error)
+    )
+  }
+
+
+}); 
+
+export const getUser = functions.https.onRequest((request, response) => {
+
+  let username = request.body.username;
+
+  let user: FirebaseFirestore.DocumentData | null = null;
+
+
+  const usersRef = admin.firestore().collection('users');
+  usersRef.where('username', '==', username).get()
+  .then(data => {
+    data.forEach(doc => {
+      user = doc.data();
+      console.log(doc.data())
+    });
+    
+    return user;
+
+  })
+  .catch(error =>
+    console.log(error)
+  )
+
+}); 
