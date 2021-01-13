98633338110695d5c6f19f85b6a2f3266e78d316
Mohamed Bengezi <bengezim@mcmaster.ca>
2021-01-13 12:21:46 -0500

    making bio text more visible

--- FrontEnd/src/screens/ProfileScreen.js
+++ FrontEnd/src/screens/ProfileScreen.js
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
         marginRight: 40,
     },
     userBioText: {
-        color: colors.grey,
+        color: colors.darkerGrey,
         fontSize: 13.5,
         textAlign: 'center',
     },
