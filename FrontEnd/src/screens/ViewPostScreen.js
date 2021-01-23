7e36c1631586c933bfc4d03d667d2e85799aed8c
Mohamed Bengezi <bengezim@mcmaster.ca>
2021-01-23 14:16:16 -0500

    Adding back button to ViewPostScreen

--- FrontEnd/src/screens/ViewPostScreen.js
+++ FrontEnd/src/screens/ViewPostScreen.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import PostDetails from '../components/PostDetails';
-
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    StatusBar
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import colors from '../hooks/colors'
+import navigate from '../navigationRef';
 const ViewPostScreen = ({ navigation }) => {
 
     const item = navigation.getParam('item');
     const title = navigation.getParam('title');
     return (
-        <PostDetails item={item} title={title} showComments={true} />
+        <View style={{ flex: 1 }}>
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.navigate("Feed");
+                }}
+                style={styles.cancel}
+
+            >
+                <Ionicons
+                    name="ios-arrow-round-back"
+                    style={styles.icon}
+                />
+            </TouchableOpacity>
+
+            <PostDetails item={item} title={title} showComments={true} />
+
+        </View>
     );
 };
+const styles = StyleSheet.create({
+    icon: {
+        color: colors.black,
+        fontWeight: "600",
+        fontSize: 50,
+    },
+    cancel: {
+        position: "absolute",
+        left: 20,
+        top: StatusBar.currentHeight,
+        width: 50,
+        height: 50
+    }
+});
 
 export default ViewPostScreen;
