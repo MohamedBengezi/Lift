1524d37f3372c73adcd8a82accb273d804c3c87f
Mohamed Bengezi <bengezim@mcmaster.ca>
2021-01-23 22:26:55 -0500

    Implementing Workout Plans screen

--- FrontEnd/src/screens/Plans/WorkoutPlansScreen.js
+++ FrontEnd/src/screens/Plans/WorkoutPlansScreen.js
@@ -1,7 +1,10 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures';
-import Button from '../../components/common/Button';
+import SearchBar from '../../components/SearchBar';
+import DropDownPicker from 'react-native-dropdown-picker';
+import colors from '../../hooks/colors';
+import PlanList from '../../components/PlanList'
 
 const reducer = (state, action) => {
     //state object and the change to make to it. 
@@ -16,12 +19,16 @@ const reducer = (state, action) => {
 
 
 const WorkoutPlansScreen = ({ navigation }) => {
+    const [search, setSearch] = useState('');
+    const [filter, setFilter] = useState('');
 
     const onSwipeDown = (gestureState) => {
         console.log('swiped down');
         navigation.navigate('Main');
     }
-
+    const searchDatabase = () => {
+        console.log('Searched')
+    }
 
     return (
         <GestureRecognizer
@@ -29,14 +36,46 @@ const WorkoutPlansScreen = ({ navigation }) => {
             style={{ flex: 1 }}
         >
             <View>
-                <Text>List Screen</Text>
+                <SearchBar
+                    term={search}
+                    onTermChange={setSearch}
+                    onTermSubmit={searchDatabase}
+                    style={styles.searchBar}
+                />
+
+                <DropDownPicker
+                    items={[
+                        { label: 'Highest Rated', value: 'rating', hidden: true },
+                        { label: 'Relevance', value: 'relevance' },
+                        { label: 'Most Popular', value: 'popular' },
+                    ]}
+                    defaultValue={filter}
+                    containerStyle={{ height: 40, width: 160, alignSelf: 'flex-end', marginRight: 10 }}
+                    style={{ backgroundColor: colors.lightGrey }}
+                    itemStyle={{
+                        justifyContent: 'flex-start', color: colors.black
+                    }}
+                    labelStyle={{ color: colors.black }}
+                    dropDownStyle={{ backgroundColor: '#fafafa' }}
+                    onChangeItem={item => setFilter(item.value)}
+                />
+
+                <PlanList navigation={navigation} />
             </View>
         </GestureRecognizer>
     );
 };
 
 const styles = StyleSheet.create({
-
+    searchBar: {
+    },
+    picker: {
+        marginRight: 20,
+        borderWidth: 2
+    },
+    plans: {
+        margin: 20
+    }
 });
 
 export default WorkoutPlansScreen;
