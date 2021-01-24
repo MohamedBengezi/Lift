235d200ead52e8047b5f2a4f2aaf237b45187925
Mohamed Bengezi <bengezim@mcmaster.ca>
2021-01-24 17:25:27 -0500

    Updating screen to use updated components

--- FrontEnd/src/screens/Feeds/RegularPostsScreen.js
+++ FrontEnd/src/screens/Feeds/RegularPostsScreen.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from "react";
 import { StyleSheet } from 'react-native';
 import serverApi from "../../api/server";
 import Feed from '../../components/Feed';
+import PostDetail from '../../components/PostDetails';
+
+import { Context as PostsContext } from '../../context/AuthContext';
 
 const apiLink = serverApi.defaults.baseURL;
 
 const RegularPostsScreen = ({ navigation }) => {
     url = apiLink + "/sample";
-
+    const { state, getUserPost } = useContext(PostsContext);
+    const [posts, setPosts] = useState(null);
     let img = (navigation.getParam('image')) ? navigation.getParam('image') : null;
     let title = (navigation.getParam('title')) ? navigation.getParam('title') : "this is a post";
 
-
+    useEffect(() => {
+        getUserPost(setPosts)
+    }, []);
+    let dummyInfo = {
+        item: {
+            username: 'Welcome',
+            caption: 'Please upload a post to get started',
+            mediaPath: "https://reactnative.dev/img/tiny_logo.png",
+            likes: 0,
+            comments_numer: 1
+        }
+    }
     return (
-        <Feed img={img} url={url} title={title} />
+        posts ? (
+            <Feed posts={posts} />
+        ) : <PostDetail item={dummyInfo} showComments={false} />
     );
 };
 
