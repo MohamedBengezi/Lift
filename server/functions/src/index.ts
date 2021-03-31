7c9be83f019ad66852e9664971916cd8719a8d46
Sagar Thomas <sagarthomas27@gmail.com>
2021-03-30 21:34:01 -0400

    add sorting logic for feedback posts. Add auto removal for feedback posts

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -4,6 +4,7 @@ import * as admin from "firebase-admin";
 import * as posts from "./https/posts";
 import * as user from "./https/user";
 import * as programs from "./https/programs";
+import * as precompute from "./triggers/precompute";
 import * as test from "./https/test";
 
 // const serviceAccount = require("../uplift-e96ab-firebase-adminsdk-fxouf-69af8f0d07.json");
@@ -21,4 +22,4 @@ const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export = { posts, helloWorld, user, programs, test };
+export = { posts, helloWorld, user, programs, test, precompute };
--- server/functions/src/triggers/precompute.ts
+++ server/functions/src/triggers/precompute.ts
@@ -1,264 +1,259 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import bfs from '../bfs';
-import Graph from '../graph';
-import Stack from '../stack';
+import bfs from "../bfs";
+import Graph from "../graph";
+import Stack from "../stack";
 
 export const precomputePlans = functions.https.onRequest(async (req, res) => {
-
-    const getDocuments = function (ref: FirebaseFirestore.QuerySnapshot): any {
-        return ref.docs.map((doc) => {
-          return {
-            id: doc.id,
-            ...doc.data(),
-          };
-        });
+  const getDocuments = function (ref: FirebaseFirestore.QuerySnapshot): any {
+    return ref.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
       };
-    
-      // Query fake users
-      const usersRef = await admin
-        .firestore()
-        .collection("users")
-        .where("fake", "==", true)
-        .get();
-    
-      const users = getDocuments(usersRef);
-    
-      const plansRef = await admin
-        .firestore()
-        .collection("workout_plans")
-        .where("fake", "==", true)
-        .get();
-    
-      const plans = getDocuments(plansRef);
+    });
+  };
 
-      const postsRef = await admin
-      .firestore()
-      .collection("general_posts")
-      .where("fake", "==", true)
-      .get();
-  
-      const posts = getDocuments(postsRef);
+  // Query fake users
+  const usersRef = await admin
+    .firestore()
+    .collection("users")
+    .where("fake", "==", true)
+    .get();
 
-      
-    
-      let G1 = buildWorkoutPlanNodes(users, plans);
-      let G2 = buildGeneralPostNodes(users, posts);
+  const users = getDocuments(usersRef);
 
-      for (let u = 0; u < users.length; u++) {
-        buildPlanRecommendations(G1, u, users, plans);
-        buildPostRecommendations(G2, u, users, posts);         
-      }
-      console.log("Built plan recommendations");
-      console.log("Built post recommendations");
+  const plansRef = await admin
+    .firestore()
+    .collection("workout_plans")
+    .where("fake", "==", true)
+    .get();
 
+  const plans = getDocuments(plansRef);
 
-    
+  const postsRef = await admin
+    .firestore()
+    .collection("general_posts")
+    .where("fake", "==", true)
+    .get();
+
+  const posts = getDocuments(postsRef);
+
+  let G1 = buildWorkoutPlanNodes(users, plans);
+  let G2 = buildGeneralPostNodes(users, posts);
+
+  const promises = [];
+
+  for (let u = 0; u < users.length; u++) {
+    promises.push(buildPostRecommendations(G2, u, users, posts));
+    promises.push(buildPlanRecommendations(G1, u, users, plans));
+  }
+
+  await Promise.all(promises);
+  console.log("Built plan recommendations");
+  console.log("Built post recommendations");
+
+  res.send("Completed recommendations");
 });
 
+function buildWorkoutPlanNodes(users: any, plans: any) {
+  // Create hashmap of unique ID's to array indices
 
-function buildWorkoutPlanNodes(users : any, plans : any) {
-    
-    // Create hashmap of unique ID's to array indices
-  
-    let vertices = new Map();
-  
-    for (let i = 0; i < users.length + plans.length; i++) {
-      if (i < users.length) {
-        vertices.set(users[i].id, i);
-      } else {
-        vertices.set(plans[i - users.length].id, i);
-      }
-    }
-  
-    console.log("Done hashmap");
-  
-    // Build users-to-workout plans graph
-  
-    let plansGraph = new Graph(vertices.size);
-  
-    for (let i = 0; i < users.length; i++) {
-      for (let j = 0; j < plans.length; j++) {
-        
-        let userFollowedPlans = users[i]!.workout_plans;
-        if(userFollowedPlans.includes(plans[i]!.id)) {
-          plansGraph.addEdge(
-            vertices.get(users[i].id),
-            vertices.get(plans[i].id)
-          );
-        }
-  
-        let categories = users[i]!.categories;
-        let tags = plans[j]!.tags;
-        if(checkTags(categories, tags)) {
-          plansGraph.addEdge(vertices.get(users[i].id), vertices.get(plans[j].id));
-        }
-  
-      }
-    }
-    console.log("Done plans graph");
+  let vertices = new Map();
 
-    return plansGraph;
+  for (let i = 0; i < users.length + plans.length; i++) {
+    if (i < users.length) {
+      vertices.set(users[i].id, i);
+    } else {
+      vertices.set(plans[i - users.length].id, i);
+    }
   }
 
-  
-  
-  function checkTags(cats: string[], tags: string[]) {
-    
-    let b = false;
-  
-    cats.forEach(c => {
-      if (tags.includes(c)) {
-        b = true;
+  console.log("Done hashmap");
+
+  // Build users-to-workout plans graph
+
+  let plansGraph = new Graph(vertices.size);
+
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < plans.length; j++) {
+      let userFollowedPlans = users[i]!.workout_plans;
+      if (userFollowedPlans.includes(plans[i]!.id)) {
+        plansGraph.addEdge(
+          vertices.get(users[i].id),
+          vertices.get(plans[i].id)
+        );
       }
-    });
-    return b;
+
+      let categories = users[i]!.categories;
+      let tags = plans[j]!.tags;
+      if (checkTags(categories, tags)) {
+        plansGraph.addEdge(
+          vertices.get(users[i].id),
+          vertices.get(plans[j].id)
+        );
+      }
+    }
   }
+  console.log("Done plans graph");
 
-  function buildPlanRecommendations(G: Graph, u : number, users : any, plans : any) {
+  return plansGraph;
+}
 
-    let numUsers = users.length;
-    let numPlans = plans.length;
+function checkTags(cats: string[], tags: string[]) {
+  let b = false;
 
-    let recs : number[] = [];
-    let b = new bfs(G,u);
+  cats.forEach((c) => {
+    if (tags.includes(c)) {
+      b = true;
+    }
+  });
+  return b;
+}
 
-    G.getAdjacent(u).forEach(plan => {
+function buildPlanRecommendations(G: Graph, u: number, users: any, plans: any) {
+  let numUsers = users.length;
+  let numPlans = plans.length;
+
+  let recs: number[] = [];
+  let b = new bfs(G, u);
+
+  G.getAdjacent(u).forEach((plan) => {
+    if (recs.length < 100) {
+      recs.push(plan);
+    }
+  });
+
+  let deg = 4;
+  loop1: while (deg < 8) {
+    loop2: for (let i = numUsers; i < numUsers + numPlans; i++) {
+      if (
+        b.hasPathTo(i) &&
+        getDegree(b.pathTo(i) || new Stack()) == deg &&
+        !recs.includes(i)
+      ) {
         if (recs.length < 100) {
-            recs.push(plan);
-        }
-    });
-
-
-    let deg = 4;
-    while (deg < 8) {
-        for (let i = numUsers; i < numUsers + numPlans; i++) {
-            if (b.hasPathTo(i) && getDegree(b.pathTo(i) || new Stack()) == deg && !recs.includes(i)) {
-                if (recs.length < 100) {
-                    recs.push(i);
-                }
-                else {return;}
-            }
-        }
-
-        deg+=2;
-    }
-
-
-    for (let i = 0; i < recs.length-1; i++) {
-        for (let j = 0; j < recs.length-i-1; j++) {
-            if (plans[recs[j]-users.length]!.rating - getDegree(b.pathTo(recs[j]) || new Stack()) > plans[recs[j+1]-users.length]!.rating - getDegree(b.pathTo(recs[j+1]) || new Stack())) {
-                let temp = recs[j];
-                recs[j] = recs[j+1];
-                recs[j+1] = temp;
-            }
-        }        
-    }
-
-    let recIDs : string[] = [];
-    for (let r = 0; r < 30; r++) {
-        let planID = plans[recs[r]-users.length]!.id;
-        recIDs.push(planID);
-    }
-
-
-    // Write to database
-    const uid = users[u]!.id;
-    let info = {recommendedPlans: recIDs};
-    admin.firestore().collection("users").doc(uid).update(info);
-  
-
-
-
-  }
-
-  function getDegree(s : Stack) {
-    return s.size();
-  }
-
-  function buildGeneralPostNodes(users : any, posts : any) {
-
-    // Create hashmap of unique ID's to array indices
-  
-    let vertices = new Map();
-  
-    for (let i = 0; i < users.length + posts.length; i++) {
-      if (i < users.length) {
-        vertices.set(users[i].id, i);
-      } else {
-        vertices.set(posts[i - users.length].id, i);
-      }
-    }
-  
-    console.log("Done hashmap");
-  
-    // Build users-to-general posts graph
-  
-    let generalPostsGraph = new Graph(vertices.size);
-  
-    for (let i = 0; i < users.length; i++) {
-      for (let j = 0; j < posts.length; j++) {
-        
-        let creator = posts[j]!.uid;
-        let following = users[i]!.following;
-        //let topic = posts[j]!.topic;
-        //let topics = posts[i]!.topics;
-  
-        if (following.includes(creator)) {
-          generalPostsGraph.addEdge(
-            vertices.get(users[i].id),
-            vertices.get(posts[j].id)
-          );
+          recs.push(i);
+        } else {
+          break loop1;
         }
       }
     }
-  
-    console.log("Done posts graph");
-    return generalPostsGraph;
+
+    deg += 2;
   }
-  
-  
-  function buildPostRecommendations(G: Graph, u : number, users : any, posts : any) {
 
-    let numUsers = users.length;
-    let numPosts = posts.length;
+  for (let i = 0; i < recs.length - 1; i++) {
+    for (let j = 0; j < recs.length - i - 1; j++) {
+      if (
+        plans[recs[j] - users.length]!.rating -
+          getDegree(b.pathTo(recs[j]) || new Stack()) >
+        plans[recs[j + 1] - users.length]!.rating -
+          getDegree(b.pathTo(recs[j + 1]) || new Stack())
+      ) {
+        let temp = recs[j];
+        recs[j] = recs[j + 1];
+        recs[j + 1] = temp;
+      }
+    }
+  }
 
-    let recs : number[] = [];
-    let b = new bfs(G,u);
+  let recIDs: string[] = [];
+  for (let r = 0; r < 30; r++) {
+    let planID = plans[recs[r] - users.length]!.id;
+    recIDs.push(planID);
+  }
 
-    G.getAdjacent(u).forEach(plan => {
+  // Write to database
+  const uid = users[u]!.id;
+  let info = { recommendedPlans: recIDs };
+  return admin.firestore().collection("users").doc(uid).update(info);
+}
+
+function getDegree(s: Stack) {
+  return s.size();
+}
+
+function buildGeneralPostNodes(users: any, posts: any) {
+  // Create hashmap of unique ID's to array indices
+
+  let vertices = new Map();
+
+  for (let i = 0; i < users.length + posts.length; i++) {
+    if (i < users.length) {
+      vertices.set(users[i].id, i);
+    } else {
+      vertices.set(posts[i - users.length].id, i);
+    }
+  }
+
+  console.log("Done hashmap");
+
+  // Build users-to-general posts graph
+
+  let generalPostsGraph = new Graph(vertices.size);
+
+  for (let i = 0; i < users.length; i++) {
+    for (let j = 0; j < posts.length; j++) {
+      let creator = posts[j]!.uid;
+      let following = users[i]!.following;
+      //let topic = posts[j]!.topic;
+      //let topics = posts[i]!.topics;
+
+      if (following.includes(creator)) {
+        generalPostsGraph.addEdge(
+          vertices.get(users[i].id),
+          vertices.get(posts[j].id)
+        );
+      }
+    }
+  }
+
+  console.log("Done posts graph");
+  return generalPostsGraph;
+}
+
+function buildPostRecommendations(G: Graph, u: number, users: any, posts: any) {
+  let numUsers = users.length;
+  let numPosts = posts.length;
+
+  let recs: number[] = [];
+  let b = new bfs(G, u);
+
+  G.getAdjacent(u).forEach((plan) => {
+    if (recs.length < 100) {
+      recs.push(plan);
+    }
+  });
+
+  let deg = 4;
+  loop1: while (deg < 8) {
+    loop2: for (let i = numUsers; i < numUsers + numPosts; i++) {
+      if (
+        b.hasPathTo(i) &&
+        getDegree(b.pathTo(i) || new Stack()) == deg &&
+        !recs.includes(i)
+      ) {
         if (recs.length < 100) {
-            recs.push(plan);
+          recs.push(i);
+        } else {
+          break loop1;
         }
-    });
-
-
-    let deg = 4;
-    while (deg < 8) {
-        for (let i = numUsers; i < numUsers + numPosts; i++) {
-            if (b.hasPathTo(i) && getDegree(b.pathTo(i) || new Stack()) == deg && !recs.includes(i)) {
-                if (recs.length < 100) {
-                    recs.push(i);
-                }
-                else {return;}
-            }
-        }
-
-        deg+=2;
+      }
     }
 
-
-    let recIDs : string[] = [];
-    for (let r = 0; r < 30; r++) {
-        let planID = posts[recs[r]-users.length]!.id;
-        recIDs.push(planID);
-    }
-
-
-    // Write to database
-    const uid = users[u]!.id;
-    let info = {recommendedPosts: recIDs};
-    admin.firestore().collection("users").doc(uid).update(info);
-    
-
+    deg += 2;
   }
+
+  let recIDs: string[] = [];
+  for (let r = 0; r < 30; r++) {
+    let planID = posts[recs[r] - users.length]!.id;
+    recIDs.push(planID);
+  }
+
+  // Write to database
+  const uid = users[u]!.id;
+  let info = { recommendedPosts: recIDs };
+  console.log(info);
+  return admin.firestore().collection("users").doc(uid).update(info);
+}
